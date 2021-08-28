e8ddbe1f8ea7cda8030c14a2ece0653e5c966c2a
Cassandra <cparisi1290@gmail.com>
2021-08-28 06:58:58 -0500

    when the key of enter is hit, the textareas input clears out after ten miliseconds. then the random selector funtion is executed

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value);
 });
 
+if (e.key === 'Enter') {
+  setTimeout(() => {
+    e.target.value = '';
+  }, 10);
+  randomSelect();
+}
+
 function createTags(input) {
   const tags = input
     .split(',')
